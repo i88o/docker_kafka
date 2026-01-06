17030ecfb7b2200fb3684cc84a684e3290d71442
i88o <playup_01@hotmail.com>
2026-01-06 14:32:45 +0800

    更新一键提交sh

--- push_to_remotes.sh
+++ push_to_remotes.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# 推送到指定的远程仓库
+push_to_remote() {
+  local remote=$1
+  echo "正在推送到 $remote..."
+  git push -u "$remote" main
+  if [ $? -ne 0 ]; then
+    echo "$remote 推送失败!"
+    exit 1
+  fi
+}
+
 # 推送到 GitHub
-echo "正在推送到 GitHub..."
-git push -u github main
-if [ $? -ne 0 ]; then
-  echo "GitHub 推送失败!"
-  exit 1
-fi
+push_to_remote github
 
 # 推送到 Gitee
-echo "正在推送到 Gitee..."
-git push -u gitee main
-if [ $? -ne 0 ]; then
-  echo "Gitee 推送失败!"
-  exit 1
-fi
+push_to_remote gitee
 
-echo "代码成功推送到 GitHub 和 Gitee！"
+# 推送到其他远程仓库
+push_to_remote gitcode
+push_to_remote gitlab
+push_to_remote gitea
+
+echo "代码成功推送到 GitHub、Gitee、GitCode、GitLab 和 Gitee！"
